f1e358a72f5b051b88e6a693137f063998646d1c
Lucas <lucaspoh7@gmail.com>
2024-12-02 23:14:46 +0800

    Remove all permission for ADMIN Role as it make an error for 403

--- C300_FYP-1/src/main/java/E63C/Lucas/LP01/WebSecurityConfig.java
+++ C300_FYP-1/src/main/java/E63C/Lucas/LP01/WebSecurityConfig.java
@@ -21,7 +21,6 @@ public class WebSecurityConfig {
 		return new BCryptPasswordEncoder();
 	}
 
-	
 	@Bean
 	public DaoAuthenticationProvider authenticationProvider() {
 		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
@@ -33,26 +32,23 @@ public class WebSecurityConfig {
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-	    http.authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
-	            // BO and FA access
-	            .requestMatchers("/Admin/Card", "/Admin/Card/Approve/*", "/Admin/Card/Reject/*", 
-	                             "/Admin/Card/Edit/*", "/Admin/Card/Delete/*", 
-	                             "/Account_type", "/Account_type/add", "/Account_type/edit/*", "/Account_type/delete/*")
-	            .hasAnyRole("BO", "FA")
-	            .requestMatchers("/Admin/consultations", "/Admin/consultations/edit/*", "/Admin/consultations/delete")
-	            .hasRole("FA")
-	            .requestMatchers("/Member", "/Member/add", "/Member/edit/*", "/Member/delete/*")
-	            .hasRole("BO")
-	            .requestMatchers("/").permitAll() // Home page is visible without logging in
-	            .requestMatchers("/register").permitAll()
-	            .requestMatchers("/forget").permitAll()
-	            .requestMatchers("/bootstrap/*/*", "/images/*").permitAll() // Static resources
-	            .anyRequest().hasRole("ADMIN"))
-	            .formLogin((login) -> login.loginPage("/login").permitAll().defaultSuccessUrl("/")) // Goes to homepage upon login
-	            .logout((logout) -> logout.logoutSuccessUrl("/")) // Goes to homepage upon logout
-	            .exceptionHandling((exceptionHandling) -> exceptionHandling.accessDeniedPage("/403"));
-
-	    return http.build();
+		http.authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
+				.requestMatchers("/Admin/Card", "/Admin/Card/Approve/*", "/Admin/Card/Reject/*", "/Admin/Card/Edit/*",
+						"/Admin/Card/Delete/*", "/Account_type", "/Account_type/add", "/Account_type/edit/*",
+						"/Account_type/delete/*")
+				.hasAnyRole("BO", "FA")
+				.requestMatchers("/Admin/consultations", "/Admin/consultations/edit/*", "/Admin/consultations/delete")
+				.hasRole("FA").requestMatchers("/Member", "/Member/add", "/Member/edit/*", "/Member/delete/*")
+				.hasRole("BO").requestMatchers("/").permitAll() // Home page is visible without logging in
+				.requestMatchers("/register").permitAll().requestMatchers("/forget").permitAll()
+				.requestMatchers("/bootstrap/*/*").permitAll() // for static resources, visible to all
+				.requestMatchers("/images/*").permitAll() // for static resources, visible to all
+				.anyRequest().authenticated())// Any other requests not specified earlier
+				.formLogin((login) -> login.loginPage("/login").permitAll().defaultSuccessUrl("/")) // Goes to homepage
+																									// upon login
+				.logout((logout) -> logout.logoutSuccessUrl("/"))// Goes to homepage upon logout
+				.exceptionHandling((exceptionHandling) -> exceptionHandling.accessDeniedPage("/403"));
+		return http.build();
 	}
 
 }
